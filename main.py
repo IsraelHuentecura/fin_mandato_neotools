99104349b6f4fe4cd2fca5371e36c9bbcfb47303
IsraelHuentecura <israel.huentecura@mayor.cl>
2024-05-28 12:09:54 -0400

    subo con categorizacion

--- main.py
+++ main.py
@@ -47,10 +47,19 @@ st.sidebar.title('Filtros')
 # Hacer columna total de miembros
 df['TotalMiembros'] = df['Socios'] + df['Socias']
 df.FinMandato = pd.to_datetime(df.FinMandato)
-# Filtrar por el número de miembros
-maxTotalMiembros = df['TotalMiembros'].max()
-miembros = st.sidebar.slider('TotalMiebros', 0, maxTotalMiembros, (0, maxTotalMiembros))
+df.FechaDepositoEstatutos = pd.to_datetime(df.FechaDepositoEstatutos)
 
+# Agregar columna con las filas en que la columna sea menor a 2017 en la columna FechaDepositoEstatutos con un valor de SI o NO
+df['NecesitaRenovacion'] = df['FechaDepositoEstatutos'].apply(lambda x: 'Si' if x < pd.to_datetime('2017-01-01') else 'No')
+# Filtra depositos de estatutos menores a 2017
+varRenovacion = st.sidebar.radio('Necesita renovación', ['Si', 'No', 'Todos'], index=2)
+# Filtrar por la columna NecesitaRenovacion
+if varRenovacion == 'Si':
+    df = df[df['NecesitaRenovacion'] == 'Si']
+elif varRenovacion == 'No':
+    df = df[df['NecesitaRenovacion'] == 'No']
+else:
+    pass
 
 # Filtro si tiene telefono o no por defecto todos
 varTelefono = st.sidebar.radio('Telefono', ['Si', 'No', 'Todos'], index=2)
@@ -69,6 +78,24 @@ elif varCorreo == 'No':
     df = df[df['Email'].isnull()]
 else:
     pass
+
+# Hacer un filtro por palabras clave 1
+palabras = st.sidebar.text_input('Palabras clave', '')
+if palabras:
+    palabras = palabras.split()
+    for palabra in palabras:
+        # Solo por nombre
+        df = df[df['nombre'].str.contains(palabra, case=False)]
+        
+# Hacer un filtro por palabras clave 2
+palabras = st.sidebar.text_input('Palabras clave 2', '')
+if palabras:
+    palabras = palabras.split()
+    for palabra in palabras:
+        # Solo por nombre
+        df = df[df['nombre'].str.contains(palabra, case=False)]
+        
+
 
 # Filtro de regiones
 dict_regiones_chile = {
@@ -116,6 +143,12 @@ else:
     varSemana = st.sidebar.slider('Vencimiento en cuantas semanas', 1, 52, 1)
     df = df[(df['FinMandato'] > hoy) & (df['FinMandato'] < hoy + pd.DateOffset(weeks=varSemana))]
 
+# Filtrar por el número de miembros
+maxTotalMiembros = df['TotalMiembros'].max()
+maxTotalMiembros = int(maxTotalMiembros)
+miembros = st.sidebar.slider('TotalMiebros', 0, maxTotalMiembros, (0, maxTotalMiembros))
+
+
 # Filtrar por el número total de miembros
 df = df[(df['TotalMiembros'] >= miembros[0]) & (df['TotalMiembros'] <= miembros[1])]
 
@@ -130,8 +163,15 @@ st.dataframe(df)
 
 # Mostrar cantidad de sindicatos filtrados
 st.write(f'Cantidad de sindicatos filtrados: {len(df)}')
+# Mostras la cantidad total de miembros de los sindicatos filtrados
+st.write(f'Total de miembros de los sindicatos filtrados: {df["TotalMiembros"].sum()}')
 
 # Filtrar outliers
 df = df[df['TotalMiembros'] < df['TotalMiembros'].quantile(0.95)]
 
 st.plotly_chart(px.histogram(df, x='TotalMiembros', nbins=100, title='Distribución de sindicatos filtrados'))
+
+# Si esta en filtro de renovación todos, mostrar un porcentaje de los sindicatos que necesitan renovación
+if varRenovacion == 'Todos':
+    porcentaje_renovacion = len(df[df['NecesitaRenovacion'] == 'Si']) / len(df) * 100
+    st.write(f'Porcentaje de sindicatos que necesitan renovación: {porcentaje_renovacion:.2f}%')
